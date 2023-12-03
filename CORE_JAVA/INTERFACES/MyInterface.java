03d587af74a2a3ba458350fb5b5476bffb36d759
RAMESH-PC <rameshbabukora@gmail.com>
2023-12-03 20:35:50 +0530

    overloading changes comit tag rameshbabukora

--- CORE_JAVA/INTERFACES/MyInterface.java
+++ CORE_JAVA/INTERFACES/MyInterface.java
@@ -1,11 +1,11 @@
 package CORE_JAVA.INTERFACES;
 
 public interface MyInterface {
-    void m1();//by default it will be like :public abstract void m1()
+    void m1();//by default, it will be like :public abstract void m1()
     public int m2(String s);
     public Student m3(int id,String name);
 
-    int x = 20;//by default it will be like :public static final int x=20;
+    int x = 20;//by default, it will be like :public static final int x=20;
 
 
 }
--- CORE_JAVA/OOPS/OverLoadingEx.java
+++ CORE_JAVA/OOPS/OverLoadingEx.java
@@ -1,14 +1,28 @@
 package CORE_JAVA.OOPS;
 
 public class OverLoadingEx {
-    public int m1(int a, int b){
-        return a+b;
+    public void m1() {
+        System.out.println("m1() called :");
     }
 
-    class B extends OverLoadingEx{
+    public void m1(int a) {
+        System.out.println("int argument m1() called :" + a);
 
     }
+
+    public void m1(int a, int b) {
+        int c = a + b;
+        System.out.println("int 2 args m1() called :" + c);
+
+    }
+
+
     public static void main(String[] args) {
+        OverLoadingEx ex = new OverLoadingEx();
+        ex.m1();
+        ex.m1(10);
+        ex.m1(10, 20);
+
 
     }
 }
